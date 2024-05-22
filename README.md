da2c275f995808c4067204a53b69b06812eec4ce
Ayolowo <58194998+Ayolowo@users.noreply.github.com>
2024-05-23 00:08:37 +0100

    First commit of code | Possible updates upcoming

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# MySQL-RDS-creation-with-terraform
+# MySQL RDS Database Deployment in VPC
+
+## Project Overview
+
+This project demonstrates the deployment of a MySQL RDS database within a Virtual Private Cloud (VPC) using AWS services. The setup ensures high availability, security, and efficient network traffic management.
+
+## Key Components
+
+1. VPC Setup
+
+- Created a Virtual Private Cloud (VPC) to host all resources.
+- Configured 2 public and 2 private subnets across multiple availability zones for redundancy and high availability.
+
+2. Database Deployment
+
+- Deployed a MySQL RDS database instance in a private subnet to ensure secure and isolated access.
+- Set up a security group for the RDS instance to control inbound and outbound traffic.
+
+3. Network Accessibility
+
+- Configured an Internet Gateway to allow outbound internet access from the VPC.
+- Set up a NAT Gateway with an Elastic IP address to enable instances in private subnets to access the internet securely.
+
+4. Security and Traffic Management
+
+- Created security groups for the RDS instance and other components to manage and control network traffic.
+- Optimized VPC and subnet configurations to ensure efficient network traffic flow and high performance.
+
+## Conclusion
+
+This setup provides a robust, secure, and high-performance infrastructure for deploying a MySQL RDS database within a VPC, leveraging AWS services to ensure scalability and reliability.
---
+++ vpc.tf
@@ -0,0 +1,115 @@
+# Create VPC
+resource "aws_vpc" "rds_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "rds_vpc"
+  }
+}
+
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+# Create 4 subnets, 2 public and 2 private
+#Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.rds_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+# Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.rds_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+
+
+
+#Create route tables for public and private subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.rds_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "public_rtb"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.rds_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    # gateway_id     = aws_internet_gateway.internet_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "private_rtb"
+    Terraform = "true"
+  }
+}
+
+#Create route table associations
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.rds_vpc.id
+  tags = {
+    Name = "internet_access_igw"
+  }
+}
+
+#Create EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "igw_eip"
+  }
+}
+
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "nat_gateway"
+  }
+}
