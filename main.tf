680ae66d76ee7ce55f558f60765e19c9f423f331
Ayolowo <58194998+Ayolowo@users.noreply.github.com>
2024-06-01 21:53:18 +0100

    Updated wrong RDS outbound rule

--- main.tf
+++ main.tf
@@ -31,10 +31,5 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 }
